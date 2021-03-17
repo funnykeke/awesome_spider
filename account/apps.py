a59bdae791223b0a3235942a2f74499d85d00da2
funny_keke <qianhulei1@gmail.com>
2021-03-17 18:56:24 +0800

    init

--- account/apps.py
+++ account/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class AcountConfig(AppConfig):
-    name = 'acount'
+    name = 'account'
---
+++ account/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def signup(request):
+    return render(request, 'signup.html')
+
+
+def signin(request):
+    return render(request, 'signin.html')
